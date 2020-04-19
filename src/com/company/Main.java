e8d2070d9c7627a05d08ba74026c0d0e70222081
Haneen <Haninabueideh@gmail.com>
2020-04-20 01:07:59 +0300

    AliHw1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner r= new Scanner(System.in);
-        int n = r.nextInt();
+        int n = r.nextInt();//ok
         int cnt=0,i=1,k=2;
         while(k<=n&&i<n)
         {
